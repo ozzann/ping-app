93737373744821f7678065eb8b7bcb9d39b603f0
anna <anna.slastnikova@gmail.com>
2016-08-02 17:02:10 +0100

    first commit

---
+++ PingServer.Tests/Test.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace PingServer.Tests
+{
+	[TestFixture]
+	public class Test
+	{
+		[Test]
+		public void TestCase()
+		{
+		}
+	}
+}
+
---
+++ PingServer/Global.asax.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.NetworkInformation;
+using System.Web;
+
+namespace PingServer
+{
+	public class Global : HttpApplication
+	{
+		protected void Application_Start()
+		{
+			string hostName = "facebook.com";
+			PingIt(hostName);
+		}
+
+		private string PingIt(string hostName)
+		{
+			Ping pingSender = new Ping();
+			int timeout = 120;
+			PingReply reply = pingSender.Send(hostName, timeout);
+			if (reply.Status == IPStatus.Success)
+			{
+				return "Hello world";
+			}
+
+			return "";
+		}
+	}
+}
